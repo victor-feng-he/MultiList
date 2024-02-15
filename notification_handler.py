122425fafcd5448439100e712f776ef68cd570fa
Victor <victorfeng2000@gmail.com>
2024-02-14 23:37:38 -0500

    Getting rid of unnecessary files

--- notification_handler.py
+++
@@ -1,7 +0,0 @@
-from plyer import notification
-
-notification.notify(
-    title='Test Notification',
-    message='This is a test notification.',
-    app_icon=None,  # You can provide the path to an icon if you have one
-)
